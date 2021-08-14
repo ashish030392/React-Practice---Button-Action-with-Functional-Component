cff1a048713f957e222c09e84c4a78c9e7ba956b
Ashish Gupta <87773898+ashish030392@users.noreply.github.com>
2021-08-14 21:17:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,23 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  
   return (
     <div id="main">
-      // Do not alter the main div
-    </div>
+				{ /* Do not remove this main div!! */ }
+    var pHidden : true;
+		<button 
+			id="click" 
+			onClick={()=>{
+				this.setState({pHidden: false });
+			}}
+		>Toggle Para Button</button>
+		{this.state.pHidden ? null : (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
+    )}
+    		</div>
   );
 }
 
