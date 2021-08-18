10d341131bb523b5433def432fa6702ffb47e883
Ashish Gupta <87773898+ashish030392@users.noreply.github.com>
2021-08-19 00:32:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,15 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+import "./../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	    
-	    this.state = { pHidden : true,
-	};
+function App() {
+  const [disp,setDisp]=useState(true);
+  return (
+    <div id="main">
+    <button id="click" onClick={()=>setDisp(false)}>show me</button>
+    {disp?null:(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
     }
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-		<button 
-			id="click" 
-			onClick={()=>{
-				this.setState({pHidden: false });
-			}}
-		>Toggle Para Button</button>
-		{this.state.pHidden ? null : (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-      </p>
-    )}
-    		</div>
-    	);
-    }
+    </div>
+  );
 }
 
 
